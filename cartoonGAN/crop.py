3eca67cca9cb14082e97d3afa84346f5179e0feb
julinas <julin0030@gmail.com>
2019-03-01 03:11:10 +0000

    edit dataset bug

--- cartoonGAN/crop.py
+++ cartoonGAN/crop.py
@@ -8,11 +8,17 @@ def cropDataFromPath(count, path, newpath):
     for fname in fnames[:count]:
       fpath = path + "/" + fname
       newfpath = newpath + "/" + fname
-      im = cv2.imread(fpath)      
-      width = int(256/im.shape[0]*im.shape[1]) # calculate goal width, assuming width>height
-      im = cv2.resize(im, (width, 256)) # resize
-      crop_index = int(im.shape[1]/2 - 256/2) # crop index so width is 256
-      im = im[:, crop_index:crop_index+256] # crop
+      im = cv2.imread(fpath)
+      if im.shape[1] > im.shape[0]:
+        width = int(256/im.shape[0]*im.shape[1])
+        im = cv2.resize(im, (width, 256)) # resize
+        crop_index = int(im.shape[1]/2 - 256/2) # crop index so width is 256
+        im = im[:, crop_index:crop_index+256] # crop
+      else:
+        height = int(256/im.shape[1]*im.shape[0])
+        im = cv2.resize(im, (256, height)) # resize
+        crop_index = int(im.shape[0]/2 - 256/2) # crop index so height is 256
+        im = im[crop_index:crop_index+256, :] # crop
       cv2.imwrite(newfpath, im)
 
 cropDataFromPath(6000, 'flickr_train', 'flickr_train256x256')
